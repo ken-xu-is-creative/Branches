f218826e0ee7929adcea76fb2ecbf204ddc59074
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-10-15 17:00:26 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: ('./home/home.module#HomePageModule')},
+  { path: 'home', loadChildren() => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'history-of-art', loadChildren: './history-of-art/history-of-art.module#HistoryOfArtPageModule' },
   { path: 'catergories-of-art', loadChildren: './catergories-of-art/catergories-of-art.module#CatergoriesOfArtPageModule' },
   { path: 'main', loadChildren: './main/main.module#MainPageModule' },
