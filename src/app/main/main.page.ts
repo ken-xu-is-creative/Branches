f4dc91bb579539b9efa4c9b5ab820f0a1fc0479f
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-10-16 14:02:51 -0400

    Version 2

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
-export class MainPage implements OnInit {
-  userDoc: any;
 
-  constructor(private fireStore: AngularFirestore) {
-    this.userDoc = fireStore.doc<any>('userProfile/we45tfgy8ij');
-  }
+
+export class MainPage implements OnInit {
+
+  constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
