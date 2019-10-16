e628bce607bf1b00cb73bd558a8f395220fd348f
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-10-16 14:44:21 -0400

    Third Message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,9 +8,63 @@ import { AngularFirestore } from 'angularfire2/firestore';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
-
+  validations_form: FormGroup;
+  errorMessage: string = '';
+ 
+  constructor(
+ 
+    private navCtrl: NavController,
+    private authService: InfoService,
+    private formBuilder: FormBuilder
+ 
+  ) { }
+ 
   ngOnInit() {
+ 
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
   }
+ 
+ 
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'pattern', message: 'Please enter a valid username.' }
+    ],
+    
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please enter a valid email.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+    ]
+  };
+ 
+ 
+  loginUser(value){
+    this.authService.loginUser(value)
+    .then(res => {
+      console.log(res);
+      this.errorMessage = "";
+      this.navCtrl.navigateForward('/dashboard');
+    }, err => {
+      this.errorMessage = err.message;
+    })
+  }
+ 
+  goToRegisterPage(){
+    this.navCtrl.navigateForward('/register');
+  }
+ 
 
 }
