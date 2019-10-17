c2308102fc70ea79bc44232d1a198f774d150b76
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-10-17 15:25:06 -0400

    3:30PM 10/17

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,45 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+ 
+    this.validations_form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+ 
+ 
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please enter a valid email.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+    ]
+  };
+ 
+ 
+  loginUser(value){
+    this.authService.loginUser(value)
+    .then(res => {
+      console.log(res);
+      this.errorMessage = "";
+      this.navCtrl.navigateForward('/dashboard');
+    }, err => {
+      this.errorMessage = err.message;
+    })
+  }
+ 
+  goToRegisterPage(){
+    this.navCtrl.navigateForward('/register');
   }
 
 }
