66c41c22ce5f6c32c469453f4bbf1ad7db6608c0
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-10-29 19:15:29 -0400

    7:15PM, 10/29

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-<<<<<<< HEAD
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-=======
-  { path: 'home', loadChildren() => import('./home/home.module').then( m => m.HomePageModule)},
->>>>>>> f218826e0ee7929adcea76fb2ecbf204ddc59074
   { path: 'history-of-art', loadChildren: './history-of-art/history-of-art.module#HistoryOfArtPageModule' },
   { path: 'catergories-of-art', loadChildren: './catergories-of-art/catergories-of-art.module#CatergoriesOfArtPageModule' },
   { path: 'main', loadChildren: './main/main.module#MainPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
   { path: 'info', loadChildren: './info/info.module#InfoPageModule' },
   { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
-  { path: 'ancient-art', loadChildren: './ancient-art/ancient-art.module#AncientArtPageModule' },
+  { path: 'ancient-art', loadChildren: './ancient-art/ancient-art.module#AncientArtPageModule' },  { path: 'username', loadChildren: './username/username.module#UsernamePageModule' },
+
     
 ];
 
