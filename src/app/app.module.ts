7efb29da5d1634eb7ec43b03375d73431129ed78
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-11-14 13:47:14 -0500

    Solved the problem with Image Upload, 11/14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule} from 'angularfire2'; 
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule, AngularFirestore} from 'angularfire2/firestore'; 
-import { firebaseConfig } from './credentials';
 import { InfoService } from '../app/info.service';
+import { ProfileServiceService } from '../app/profile-service.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import * as firebase from 'firebase';
@@ -28,19 +28,22 @@ firebase.initializeApp(environment.firebase);
   imports: [BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(environment),
     AngularFirestoreModule,
     AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
     InfoService,
+    ProfileServiceService,
     AngularFireStorage,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
 
 
 
---
+++ src/app/profile-image/profile-image.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-image',
+  templateUrl: './profile-image.page.html',
+  styleUrls: ['./profile-image.page.scss'],
+})
+export class ProfileImagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
