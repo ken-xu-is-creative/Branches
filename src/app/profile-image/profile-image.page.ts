d7cdd754549ea36b0e239a56cef7ea54104967d8
ken-xu-is-creative <55454948+ken-xu-is-creative@users.noreply.github.com>
2019-11-17 19:07:10 -0500

    Basic Functionality

--- src/app/profile-image/profile-image.page.ts
+++ src/app/profile-image/profile-image.page.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { uploadAvatar} from '../profile-service.service';
+import * as firebase from 'firebase';
+import { NavController } from '@ionic/angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+export interface Image {
+  id: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-profile-image',
   templateUrl: './profile-image.page.html',
   styleUrls: ['./profile-image.page.scss'],
 })
+
 export class ProfileImagePage implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private navCtrl: NavController,
+    private afs: AngularFirestore) { 
+  }
+
+  loading = false;
+  downloadURL: any;
+  userEmail: string;
+
+  url: any;
+  avatarImage: Image = {
+    id: this.afs.createId(), image: ''
+  };
 
   ngOnInit() {
+  }
+
+  uploadImage(event) {
+    const user = firebase.auth().currentUser;
+    console.log(user.email);
+
+    this.loading = false;
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+      // For Preview Of Image
+      reader.onload = (e: any) => { // called once readAsDataURL is completed
+        this.url = e.target.result;
+
+        // For Uploading Image To Firebase
+        const fileraw = event.target.files[0];
+        console.log(fileraw);
+
+        uploadAvatar(fileraw);
+
+      
+    }
+  }
+}
+
+
+  
+
+  GoMainPage(){
+
+    this.navCtrl.navigateBack('/main');
+
   }
 
 }
